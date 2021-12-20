afa51aaa3968af951641bdca029f6f9e6700383c
HsuehBenito <bhsueh94@gmail.com>
2021-12-20 20:08:42 -0300

    fin

--- app.js
+++ app.js
@@ -3,16 +3,22 @@ const path = require('path');
 const app = express();
 
 app.get('', (req,res) =>{
-    res.send("Toy dentro"); 
+    res.send("Iniciado"); 
 });
 
 app.get('/mercadoliebre', (req,res) =>{
     
     res.sendFile((__dirname + '/views/home.html'));
 });
+
 app.get('/crearcuenta', (req,res) =>{
     
     res.sendFile((__dirname + '/views/register.html'));
+});
+
+app.get('/ingresar', (req,res) =>{
+    
+    res.sendFile((__dirname + '/views/login.html'));
 });
 
 app.use(express.static(path.join(__dirname, './public')));  
